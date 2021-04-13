f4caf231948ac7a7bd5207f3e02ea56ad962309a
S11as <50956430+S11as@users.noreply.github.com>
2021-04-13 19:37:23 +0300

    lab2 init

---
+++ Tomogramm/Tomogramm/Tomogramm/View.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace Tomogramm
+{
+    class View
+    {
+        public View() { }
+
+        public void SetupView(int width, int height)
+        {
+            GL.ShadeModel(ShadingModel.Smooth);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(0, Bin.X, 0, Bin.Y, -1, 1);
+            GL.Viewport(0, 0, width, height);
+        }
+    }
+}
